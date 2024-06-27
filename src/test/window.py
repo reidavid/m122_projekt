944a7c5c00de12bb0fe85cd70f014bafafdc3ef1
reidavid <david.felicio@edu.tbz.ch>
2024-06-27 08:57:08 +0200

    tkinter testen

--- src/test/window.py
+++ src/test/window.py
@@ -2,12 +2,44 @@ import tkinter as tk
 from tkinter import *
 
 
+def button_pressed(i):
+    Window(i, 500, 200)
+
+
+class Window(Tk):
+    def __init__(self, name, w, h):
+        super().__init__()
+        self.title(name)
+        ws = self.winfo_screenwidth()
+        hs = self.winfo_screenheight()
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.resizable(False, False)
+
+
 window = tk.Tk()
 
 greeting = tk.Label(
     text="hallo",
-    foreground="black",
-    background="white"
+    fg="black",
+    background="#34A2FE",
+    height=10,
+    width=50
 )
 
-greeting.mainloop()
+btn = tk.Button(
+    text="hallo",
+    width=25,
+    height=5,
+    bg="blue",
+    fg="yellow",
+    command=lambda m="test": button_pressed(m)
+)
+
+greeting.pack(fill=tk.Y, side=tk.LEFT)
+btn.pack(fill=tk.X)
+
+window.mainloop()
+
+print("test")
