27a29a760d5121520295fad967235db2032a1912
reidavid <david.felicio@edu.tbz.ch>
2024-07-01 15:53:17 +0200

    Konvertierung von Txt zu PDF testen

--- src/test/convert_pdf.py
+++ src/test/convert_pdf.py
@@ -1,13 +1,24 @@
-#!/usr/bin/env python
-
-import pypandoc
-import docx
+from fpdf import FPDF
 
 
 class ConvertPDF:
+    def __init__(self, data):
+        pdf = FPDF()
 
-    def __init__(self, txt):
-        doc = docx.Document
-        doc.add_heading(txt)
-        doc.save('tmp_doc.docx')
-        pypandoc.convert_file('tmp_doc.docx', docx)
+        # Add a page
+        pdf.add_page()
+
+        # set style and size of font
+        # that you want in the pdf
+        pdf.set_font("Arial", size = 15)
+
+        # open the text file in read mode
+        f = open("%tmp.txt", "w+")
+        f.write(data)
+
+        # insert the texts in pdf
+        for x in f:
+            pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
+
+        # save the pdf with name .pdf
+        pdf.output("asdf.pdf")
